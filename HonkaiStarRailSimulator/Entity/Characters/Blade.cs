06c17cf17d78bbc395ef9338c90453b210ed12da
Chris George <chrismathew98@gmail.com>
2023-11-19 00:01:31 -0500

    Implemented Blade DoAction. Debug Damage boost from Bronya.

--- HonkaiStarRailSimulator/Entity/Characters/Blade.cs
+++ HonkaiStarRailSimulator/Entity/Characters/Blade.cs
@@ -5,4 +5,11 @@ public class Blade : Character
     public Blade(int level = 80) : base(CharacterId.Blade, level)
     {
     }
+
+    public override void DoAction()
+    {
+        Console.WriteLine($"DMG Boost: {GetTotalDamageBoost(Some<Element>.Of(Element.Wind), DamageBonusType.All, DamageBonusType.BasicAttack)}");
+        // Do Nothing
+        FinishTurn();
+    }
 }
