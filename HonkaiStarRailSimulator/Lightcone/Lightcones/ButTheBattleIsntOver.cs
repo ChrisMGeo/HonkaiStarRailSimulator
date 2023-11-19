411c35a307e8151da2bf37a9d65ce40281413968
Chris George <chrismathew98@gmail.com>
2023-11-19 12:36:32 -0500

    Added Bronya LC.

---
+++ HonkaiStarRailSimulator/Lightcone/Lightcones/ButTheBattleIsntOver.cs
@@ -0,0 +1,20 @@
+﻿namespace HonkaiStarRailSimulator.Lightcones;
+
+public class ButTheBattleIsntOver : Lightcone
+{
+    private readonly StatusEffect _errBoost;
+    public ButTheBattleIsntOver(int level = 80) : base(LightconeId.ButTheBattleIsnTOver, level)
+    {
+        
+    }
+
+    public override void AttachTo(Character c)
+    {
+        base.AttachTo(c);
+    }
+
+    public override void DetachFrom(Character c)
+    {
+        base.DetachFrom(c);
+    }
+}
